1e87cb84706548ab42a3c7e0c950a9436d584482
LuisCR91 <luisalfredo.rodriguez.cespedes@ucreativa.com>
2021-07-25 13:18:29 -0600

    Actualizando código
    Calculadora monolítica server

---
+++ calculadora-monolitica-server/src/main/java/com/ucreativa/calculadoramonoliticaserver/CalculadoraMonoliticaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ucreativa.calculadoramonoliticaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMonoliticaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraMonoliticaServerApplication.class, args);
+	}
+
+}
