53281285062cb5edae9a0d87642fe763369788ca
LuisCR91 <luisalfredo.rodriguez.cespedes@ucreativa.com>
2021-07-25 12:47:46 -0600

    Subiendo código
    Código de la calculadora monolítica

---
+++ calculadora-monolitico/src/main/java/com/ucreativa/calculadoramonolitico/CalculadoraMonoliticoApplication.java
@@ -0,0 +1,13 @@
+package com.ucreativa.calculadoramonolitico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMonoliticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraMonoliticoApplication.class, args);
+	}
+
+}
---
+++ calculadora-monolitico/src/test/java/com/ucreativa/calculadoramonolitico/CalculadoraMonoliticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucreativa.calculadoramonolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraMonoliticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
