90101407d1a3952e1adb61825a738e186cd2ae0f
LuisCR91 <luisalfredo.rodriguez.cespedes@ucreativa.com>
2021-08-12 19:22:21 -0600

    Proyecto paso 3
    Actualizando con el paso 3 del proyecto final.

---
+++ service-suma/src/main/java/com/example/servicesuma/ServiceSumaApplication.java
@@ -0,0 +1,24 @@
+package com.example.servicesuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServiceSumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSumaApplication.class, args);
+	}
+	@GetMapping ("/suma/{num1}/mas/{num2}")
+	
+			public String suma (@PathVariable String num1, @PathVariable String num2) 
+			{
+				Integer result = Integer.parseInt(num1) + Integer.parseInt(num2);
+				return result.toString(); 
+			}
+
+}
---
+++ service-suma/src/test/java/com/example/servicesuma/ServiceSumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicesuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
